c6829e041a77cd8494d4ac7a657e85107a5688a8
sheema <sheemashamilfarook@gmail.com>
2023-03-21 17:47:59 +0300

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,10 @@
+
+
+class ReviewsController < ApplicationController
+
+    def index
+      reviews = Review.all.order(rating: :desc)
+      render json: reviews, include: :dog_house 
+    end
+  
+  end
